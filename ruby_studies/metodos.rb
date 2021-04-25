4286d7f290f3ed1d0f7b0d78e1f5b01c1b232097
williamrichaard <wrichaard@hotmail.com>
2021-04-25 20:56:35 -0300

    ruby methods

---
+++ ruby_studies/metodos.rb
@@ -0,0 +1,25 @@
+#Esquema de code blocks (Bloco de códigos)
+
+#No ruby a boa prática de programação é usar o padrão snake_case para variáveis e metódos
+#snake_case
+
+#Metódo snake_case__nome:
+def snake_case_nome
+    puts "William Richard"
+end
+
+#Para usar o metódo ele precisa ser invocado:
+snake_case_nome
+
+# --------------------------------------------------------------------------------------- #
+
+#Customizando metódo para imprimir qualquer tipo de nome:
+
+def snake_case_nome(nome) #argumento para o metódo
+    puts nome
+end
+
+snake_case_nome("William")
+
+# --------------------------------------------------------------------------------------- #
+
