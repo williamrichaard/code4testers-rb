49a82f42dc9e85c99c58a8599be0fdb5c95e44ae
williamrichaard <wrichaard@hotmail.com>
2021-04-25 20:28:39 -0300

    ruby types

---
+++ ruby_studies/tipos.rb
@@ -0,0 +1,15 @@
+#Ruby trabalha com linguagem dinâmica
+
+nome = "William" #variável
+idade = 26       #variável
+legal = true     #variável
+
+#Imprimindo as variáveis
+puts nome
+puts nome.class #class é para trazer o tipo de variável
+
+puts idade
+puts idade.class
+
+puts legal
+puts legal.class
