8c9e9f8be1dac4d3274c38a1de56a9d7ed8996c3
williamrichaard <wrichaard@hotmail.com>
2021-04-25 21:33:24 -0300

    ruby methods

--- ruby_studies/metodos.rb
+++ ruby_studies/metodos.rb
@@ -39,3 +39,12 @@ def snake_case_soma(valor1, valor2)
 end
 
 puts snake_case_soma(2, 4)
+
+# --------------------------------------------------------------------------------------- #
+
+#5 O ruby sempre devolve automaticamente a última instrução de código, exemplo:
+def snake_case_soma(valor1, valor2)
+    valor1 + valor2  # <-- útilma instrução
+end
+
+puts snake_case_soma(2, 4)
