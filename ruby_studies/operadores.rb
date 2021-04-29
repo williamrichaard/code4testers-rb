ef7b2ab137e3e21a8ebe6693884f36463abb4c4a
williamrichaard <wrichaard@hotmail.com>
2021-04-29 07:15:38 -0300

    ruby operators

--- ruby_studies/operadores.rb
+++ ruby_studies/operadores.rb
@@ -1,7 +1,36 @@
 #Operadores Matemáticos
 
+#SOMA
 def soma(n1, n2)
 	puts n1 + n2
 end
 
 soma(2, 3)
+
+#SUBTRAÇÃO
+def subtrai(n1, n2)
+	puts n1 - n2
+end
+
+subtrai(2, 3)
+
+#MULTIPLICAÇÃO
+def multiplica(n1, n2)
+	puts n1 * n2
+end
+
+multiplica(2, 3)
+
+#DIVISÃO
+def divide(n1, n2)
+	puts n1.to_f / n2.to_f #to_f é um metódo nativo do ruby que converte para float
+end
+
+divide(10, 3)
+
+#DIVISÃO
+def divide(n1, n2)
+	puts (n1.to_f / n2.to_f).round(2) #round é um metódo nativo do ruby que arredonda o resultado
+end
+
+divide(10, 3)
