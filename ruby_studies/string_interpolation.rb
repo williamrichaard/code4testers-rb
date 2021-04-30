74101e33abf0348be87e930d3a04cd72e67f0fc9
williamrichaard <wrichaard@hotmail.com>
2021-04-30 19:00:21 -0300

    string_interpolation

---
+++ ruby_studies/string_interpolation.rb
@@ -0,0 +1,13 @@
+# Interpolão de Stings
+
+puts "Qual o seu nome?"
+nome = gets.chomp # chomp remove a quebra de linha, fazendo a concatenação adequada
+puts "Informe a sua idade?"
+idade = gets.chomp.to_i # to_i coverte de string para inteiro
+
+if (idade >= 18)
+  # Concatenado
+  puts "#{nome}, você tem #{idade} anos e pode tirar carteira de motorista."
+else
+  puts "#{nome}, você tem #{idade} anos, então melhor continuar andando de bicicleta."
+end
