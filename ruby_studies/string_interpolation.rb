82231f1f3f223ef50600ab8500b9eccb5a7161b9
williamrichaard <wrichaard@hotmail.com>
2021-04-30 19:01:17 -0300

    string_interpolation

--- ruby_studies/string_interpolation.rb
+++ ruby_studies/string_interpolation.rb
@@ -6,7 +6,6 @@ puts "Informe a sua idade?"
 idade = gets.chomp.to_i # to_i coverte de string para inteiro
 
 if (idade >= 18)
-  # Concatenado
   puts "#{nome}, você tem #{idade} anos e pode tirar carteira de motorista."
 else
   puts "#{nome}, você tem #{idade} anos, então melhor continuar andando de bicicleta."
