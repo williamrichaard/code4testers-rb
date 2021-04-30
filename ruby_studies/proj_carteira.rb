5f087a5e6dfe540b8929514946fac40800ee641b
williamrichaard <wrichaard@hotmail.com>
2021-04-30 18:35:57 -0300

    driver's license program

--- ruby_studies/proj_carteira.rb
+++ ruby_studies/proj_carteira.rb
@@ -1,11 +1,21 @@
 # Controle de fluxos
 
 nome = "William"
-idade = 26
+idade = 8
 
 if (idade >= 18)
   # Concatenado
   puts nome + ", você pode tirar carteira de motorista."
 else
-  puts nome + ", melhor continuar andando de bicicleta"
+  puts nome + ", melhor continuar andando de bicicleta."
+end
+
+# ---------------------------------------------------------------#
+
+if (idade >= 18)
+  puts nome + ", você pode tirar carteira de motorista."
+elsif (idade >= 7)
+  puts nome + ", melhor continuar andando de bicicleta."
+else
+  puts nome + ", você é muito jovem e só pode andar de motoquinha."
 end
