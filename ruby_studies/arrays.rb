65189b8388406e3ae67bb26a56bd172d07f89291
williamrichaard <wrichaard@hotmail.com>
2021-04-30 19:17:02 -0300

    ruby arrays

--- ruby_studies/arrays.rb
+++ ruby_studies/arrays.rb
@@ -1,11 +1,15 @@
 bandas = ["AC/DC", "Black Sabbath", "Tenacious D", "Queen", "Molejão"]
 
-puts bandas[0]
-puts bandas[1]
-puts bandas[2]
-puts bandas[3]
-puts bandas[4]
+# puts bandas[0]
+# puts bandas[1]
+# puts bandas[2]
+# puts bandas[3]
+# puts bandas[4]
 
-bandas.push("Cleane dos Teclados")
+bandas.push("Cleane dos Teclados") # push é um método que funciona para variáveis do tipo array para adicionar novos elementos
 
-puts bandas[5]
+# puts bandas[5]
+
+bandas.delete("Queen") # delete remove um elemento do array
+
+puts bandas
