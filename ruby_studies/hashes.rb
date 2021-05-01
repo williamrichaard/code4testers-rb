d1b3044143e2db796f3a5d9dcd59192b8cdf41a5
williamrichaard <wrichaard@hotmail.com>
2021-05-01 07:23:35 -0300

    ruby arrays

--- ruby_studies/hashes.rb
+++ ruby_studies/hashes.rb
@@ -2,6 +2,16 @@
 
 # No hash temos acesso as informações por meio de chaves
 william = { nome: "William", email: "wrichard@hotmail.com", legal:true }
-puts william[:nome]
-puts william[:email]
-puts william[:legal]
+# puts william[:nome]
+# puts william[:email]
+# puts william[:legal]
+
+cleane = { nome: "Cleane", email: "cleane@hotmail.com", legal:true }
+# puts cleane[:nome]
+# puts cleane[:email]
+# puts cleane[:legal]
+
+pessoas = [william, cleane]
+
+puts pessoas[0][:nome]
+puts pessoas[1][:email]
