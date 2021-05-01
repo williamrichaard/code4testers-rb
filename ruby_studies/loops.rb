37cf377cf8cea5c7372de50602997f2ef159b430
williamrichaard <wrichaard@hotmail.com>
2021-05-01 08:11:40 -0300

    ruby loops times

--- ruby_studies/loops.rb
+++ ruby_studies/loops.rb
@@ -7,3 +7,11 @@
 #   i = i + 1
 # end
 
+bandas = ["Roupa Nova", "Os Paralamas do Sucesso", "Paramore", "Tenacious D"]
+
+# size é um método que funciona para arrays e retorna a quantidade de items do array
+i = 0
+bandas.size.times do
+  puts bandas[i]
+  i = i + 1
+end
